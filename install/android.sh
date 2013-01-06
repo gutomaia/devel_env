a52a1a570fd6bbf59d8101c64e32833e1dceb3ce
Guto Maia <guto@guto.net>
2013-01-06 16:41:15 -0200

    addded symbolic link for android-sdk path

--- install/android.sh
+++ install/android.sh
@@ -11,4 +11,6 @@ if [ "$#" -eq 5 ]; then
 		unzip $DOWNLOAD_PATH/$P_FILE
 
 	$DEVEL_HOME/$P_DIR/tools/android update sdk --filter tools,platform-tools,android-17,android-16,android-15,android-14,android-13,android-12,android-11,android-10 --no-ui --force
+
+	ln -s $P_INSTALL_DIR $P_NAME
 fi
