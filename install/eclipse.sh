fac133f903f65c3e3ff7b2bc83f6222197356f69
Guto Maia <guto@guto.net>
2013-01-06 14:38:23 -0200

    added eclipse install script

---
+++ install/eclipse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -eq 5 ]; then
+	P_NAME="$1"
+	P_FILE="$2"
+	P_INSTALL_DIR="$3"
+	DEVEL_HOME="$4"
+	DOWNLOAD_PATH="$5"
+
+	cd $DEVEL_HOME && \
+		tar -zxvf $DOWNLOAD_PATH/$P_FILE
+	
+	#install maven plugin
+	$DEVEL_HOME/eclipse/eclipse -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/juno/ -installIU org.eclipse.m2e.feature.feature.group -nosplash
+	#install git plugin
+	$DEVEL_HOME/eclipse/eclipse -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/juno/ -installIU org.eclipse.egit.feature.group -nosplash
+	
+	#install android adt plugin
+	$DEVEL_HOME/eclipse/eclipse -application org.eclipse.equinox.p2.director -repository https://dl-ssl.google.com/android/eclipse -installIU com.android.ide.eclipse.adt.feature.group -nosplash
+	#install android ndk plugin
+	$DEVEL_HOME/eclipse/eclipse -application org.eclipse.equinox.p2.director -repository https://dl-ssl.google.com/android/eclipse -installIU com.android.ide.eclipse.ndk.feature.group -nosplash
+	#TODO: $DEVEL_HOME/eclipse/eclipse -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/juno/ -installIU com.googlecode.eclipse.m2e.android.feature.feature.group
+fi
