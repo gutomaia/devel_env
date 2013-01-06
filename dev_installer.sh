80f09d8ae7d020138edebd9c3c87868c8e4224d5
Guto Maia <guto@guto.net>
2013-01-06 16:39:49 -0200

    put if and then on the same line

--- dev_installer.sh
+++ dev_installer.sh
@@ -8,13 +8,11 @@ DEVEL_HOME=~/devel
 PROGRAMS=$(find downloads/)
 for P in $PROGRAMS;
 do
-	if [ -f $P ]
-	then
+	if [ -f $P ]; then
 		P_NAME=$(echo $P | cut -d\/ -f2)
 		P_FILE=$(cat $P | cut -d\| -f1)
 		P_URL=$(cat $P | cut -d\| -f3)
-		if [ ! -f $DOWNLOAD_PATH/$P_FILE ]
-		then
+		if [ ! -f $DOWNLOAD_PATH/$P_FILE ]; then
 			wget --no-check-certificate $P_URL -O $DOWNLOAD_PATH/$P_FILE
 		else
 			#todo check md5
@@ -26,14 +24,12 @@ done
 #install programs
 for P in $PROGRAMS;
 do
-	if [ -f $P ]
-	then
+	if [ -f $P ]; then
 		P_NAME=$(echo $P | cut -d\/ -f2)
 		P_FILE=$(cat $P | cut -d\| -f1)
 		P_INSTALL_DIR=$(cat $P | cut -d\| -f2)
 		P_URL=$(cat $P | cut -d\| -f3)
-		if [ ! -d $DEVEL_HOME/$P_INSTALL_DIR -a -f install/$P_NAME.sh  ]
-		then
+		if [ ! -d $DEVEL_HOME/$P_INSTALL_DIR -a -f install/$P_NAME.sh  ]; then
 			sh install/$P_NAME.sh $P_NAME $P_FILE $P_INSTALL_DIR $DEVEL_HOME $DOWNLOAD_PATH
 		fi
 	fi
